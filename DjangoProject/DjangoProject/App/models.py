2bbcc550262218d8dc329835fef78695100b3424
Kristiyan Pisev <114045349+KristiyanP-03@users.noreply.github.com>
2025-01-31 20:13:44 +0200

    New Project
    I start this project with the goal to make full stack deployed site for under 1 week. It is essential when I try to find work as program developer, it is university assignment as well.

--- DjangoProject/DjangoProject/App/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Profile(models.Model):
-    #'first_name', 'last_name','month', 'day', 'year', 'gender','email','username', 'password', 'confirm_password'
-    pass
--- DjangoProject/DjangoProject/App/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from DjangoProject.App.views import *
-
-urlpatterns = [
-    path("", home),
-    path("profile/create/", profile_creation, name="")
-]
